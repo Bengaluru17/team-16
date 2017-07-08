aeec07bc4944fc78385d4a455d5806b8e99c5dc6
Gopan Samantaray <gsamantaray.1102@gmail.com>
2017-07-08 17:00:00 +0530

    Create views for login

--- Amogh/login/views.py
+++ Amogh/login/views.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
+from django.contrib.auth import login
 from django.shortcuts import render
+from .forms import LoginForm
+from .models import User
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.filter(name=cd['username'], password=cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+
+                else:
+                    return HttpResponse('Disabled account')
+
+            else:
+                return HttpResponse('Invalid login')
+
+        else:
+            form = LoginForm()
+        return render(request, 'login.html', {'form': form})
