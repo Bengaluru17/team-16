ee81293a7df9581bd08e395c096b3cd2bf241fca
Gopan Samantaray <gsamantaray.1102@gmail.com>
2017-07-08 15:18:52 +0530

    Add default value to password in models of User

--- Amogh/login/models.py
+++ Amogh/login/models.py
@@ -6,12 +6,11 @@ from django.db import models
 class User(models.Model):
   name = models.CharField(max_length=100, blank=True, null=True)
   email = models.CharField(max_length=100, blank=True, null=True)
-  password = models.CharField(max_length=100, blank=True, null=True)
+  password = models.CharField(max_length=100, blank=True, null=True, default='qwerty')
   isAdmin = models.BooleanField()
   score = models.IntegerField()
   level = models.IntegerField()
 
   def __str__(self):
     return self.name
-  
   
