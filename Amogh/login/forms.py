1ef52de1e4022e33b9754c97cc7e1deebcd73edf
Gopan Samantaray <gsamantaray.1102@gmail.com>
2017-07-08 16:19:22 +0530

    Import validate_email from django.core.validators in forms file in login

--- Amogh/login/forms.py
+++ Amogh/login/forms.py
@@ -1,10 +1,14 @@
+from django.core.validators import validate_email
+
 from django import forms
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username')
-    password = forms.CharField(widget=forms.PasswordInput,label='Password')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+
 
 class SignupForm(forms.Form):
     username = forms.CharField(label='Username')
-    password = forms.CharField(widget=forms.PasswordInput,label='Password')
-    email = forms.EmailField(label='Password',validators=[validate_email])
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    email = forms.EmailField(label='Password', validators=[validate_email])
