d095f544a2722ebc455d25315d4d2a34f667de70
Gopan Samantaray <gsamantaray.1102@gmail.com>
2017-07-08 15:34:10 +0530

    Add tests to models and unique as true to name and email

--- Amogh/login/models.py
+++ Amogh/login/models.py
@@ -4,13 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-  name = models.CharField(max_length=100, blank=True, null=True)
-  email = models.CharField(max_length=100, blank=True, null=True)
-  password = models.CharField(max_length=100, blank=True, null=True, default='qwerty')
+  name = models.CharField(max_length=100, unique=True)
+  email = models.CharField(max_length=100, unique=True)
+  password = models.CharField(max_length=100, default='qwerty')
   isAdmin = models.BooleanField()
   score = models.IntegerField()
   level = models.IntegerField()
-
+  test2 = models.IntegerField()
+  test3 = models.IntegerField()
+  test4 = models.IntegerField()
+  test5 = models.IntegerField()
+  test6 = models.IntegerField()
+  test7 = models.IntegerField()
+  test8 = models.IntegerField()
+  test9 = models.IntegerField()
+  test10 = models.IntegerField()
+  test = models.IntegerField()
+  
   def __str__(self):
     return self.name
   
